9815e884195cff88fd9a3e527353b758c5887dc5
codingwithchitra <cancoding@gmail.com>
2024-07-29 21:52:20 +0700

    Guessing Games

--- GuessingNumber/NumberGuessingGame.java
+++ GuessingNumber/NumberGuessingGame.java
@@ -1,5 +1,35 @@
+
+import java.util.Random;
+import java.util.Scanner;
 public class NumberGuessingGame {
     public static void main(String[] args) {
-        
+        Random rn = new Random();
+        Scanner sc = new Scanner(System.in);
+        boolean isCorrect = false; // guessing
+        int attempt = 0;
+        System.out.println(" Welcome to guessing number.");
+        System.out.println("=================================");
+        int sysnum = rn.nextInt(3) + 1;
+        while(! isCorrect){ //while loop
+            System.out.print("Please Enter the number from 0 to 100= ");
+            
+            int guessNutmber = sc.nextInt();
+
+            if(guessNutmber < sysnum){
+                System.out.println("Your guess is too small!");
+            }else if(guessNutmber > sysnum){
+                System.out.println(" Your guess is to bigger!");
+            }else{
+                System.out.println("Congratulation you did correct!");
+                isCorrect = true;
+            }
+
+            if(attempt == 5){
+                System.out.println("Your guess is reaching limitted!");
+                break;
+            }
+            attempt ++;
+        }
+        sc.close();
     }
 }
